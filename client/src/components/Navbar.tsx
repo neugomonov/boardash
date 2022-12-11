24bc9f7437ee4969f10c8d56577716367748b7a2
neugomonov <neugomonovv@gmail.com>
2022-12-11 04:47:50 +0300

    🧭 Sidebar (done)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,7 +19,7 @@ import {
   useTheme,
 } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
   return (
@@ -27,7 +27,7 @@ const Navbar = () => {
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
