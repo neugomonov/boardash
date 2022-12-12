05110a321bf79df74dc496f1ba1c1c646e5e199c
neugomonov <neugomonovv@gmail.com>
2022-12-12 23:55:39 +0300

    🌿 Products info inserted (added)
    🛒 part of - products page

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -38,6 +40,8 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
     /* ONLY ADD DATA ONE TIME */
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
     // User.insertMany(dataUser);
   })
   .catch((error) => console.error(`${error} did not connect`));
