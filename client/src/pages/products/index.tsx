c2f39a8cb382f956863f9483b6001168ad00c364
neugomonov <neugomonovv@gmail.com>
2022-12-13 23:49:38 +0300

    🛒 products page (wip)

---
+++ client/src/pages/products/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
