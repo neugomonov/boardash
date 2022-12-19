0f96f86830fe5089c3ad4eb9d13ec1b51cae4a53
neugomonov <neugomonovv@gmail.com>
2022-12-20 00:26:19 +0300

    🎛 controllers - management & general (added)
    🗣 📈 🖥 part of: admin & performance & dashboard pages

--- server/controllers/general.ts
+++ server/controllers/general.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
 export const getUser = async (req: Request, res: Response) => {
@@ -6,6 +8,47 @@ export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req: Request, res: Response) => {
+  try {
+    // hardcoded values
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+    /* Recent Transactions */
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdAt: -1 });
+    /* Overall Stats */
+    const overallStat = await OverallStat.find({ year: currentYear });
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+    const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+    const todayStats = overallStat[0].dailyData.find(({ date }) => {
+      return date === currentDay;
+    });
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
