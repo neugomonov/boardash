99861b7f7cec6d00cf4dc9616f96a957500ea5e6
Valery Neugomonov <neugomonovv@gmail.com>
2022-12-22 04:20:00 +0600

    🔻 framer motion buttons wrapper (added)

---
+++ client/src/components/motion/MainContentMotionWrapper.tsx
@@ -0,0 +1,34 @@
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+export default function MainContentMotionWrapper({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <motion.div
+      exit="hidden"
+      initial="appearing"
+      animate="visible"
+      variants={{
+        hidden: {
+          y: "-1vh",
+          x: ["0vw", "-100vw"],
+          scale: 0.9,
+          position: "absolute",
+          zIndex: -1,
+          transition: { type: "spring", bounce: 0.3, duration: 0.8 },
+        },
+        appearing: { y: window.innerHeight },
+        visible: {
+          y: 0,
+          scale: 1,
+          transition: { type: "spring", bounce: 0.3 },
+        },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
