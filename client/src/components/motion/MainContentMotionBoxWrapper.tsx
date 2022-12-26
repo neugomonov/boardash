eef78884ea1db2add6af63ad0d51db244504ad38
Valery Neugomonov <neugomonovv@gmail.com>
2022-12-26 23:45:06 +0600

    🔻 MainContentMotionBoxWrapper (animation)

--- client/src/components/motion/MainContentMotionBoxWrapper.tsx
+++ client/src/components/motion/MainContentMotionBoxWrapper.tsx
@@ -2,33 +2,32 @@ import { Box } from "@mui/material";
 import { motion } from "framer-motion";
 import React, { ReactNode } from "react";
 
-const MainContentMotionBoxWrapper = ({
-  children,
-  key,
-}: {
-  children: ReactNode;
-  key: string;
-}) => {
+const MainContentMotionBoxWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <Box
-      m="1.5rem 2.5rem"
-      component={motion.div}
-      key={key}
+    <motion.div
       initial="appearing"
       animate="visible"
+      exit="hidden"
       variants={{
-        appearing: { opacity: 0 },
+        appearing: {
+          opacity: 0,
+        },
         visible: {
           opacity: 1,
           transition: {
-            duration: 1,
+            duration: 0.2,
+          },
+        },
+        hidden: {
+          opacity: 0,
+          transition: {
+            duration: 0.2,
           },
         },
       }}
-      exit={{ opacity: 0 }}
     >
-      {children}
-    </Box>
+      <Box m="1.5rem 2.5rem">{children}</Box>
+    </motion.div>
   );
 };
 
