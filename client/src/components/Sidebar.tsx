f899987d3d85202fd2143aca1f7c8431fbf9feee
Valery Neugomonov <neugomonovv@gmail.com>
2022-12-27 23:16:47 +0600

    🧭 SwipeableDrawer (added)

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -17,6 +17,7 @@ import {
 import {
   Box,
   Divider,
+  Drawer,
   IconButton,
   List,
   ListItem,
@@ -108,168 +109,292 @@ const Sidebar = ({
     setActive(pathname.substring(1));
   }, [pathname]);
   return (
-    <AnimatePresence>
-      {isSidebarOpen ? (
-        <motion.div
-          key="sidebarContent"
-          initial="appearing"
-          animate="visible"
-          exit="appearing"
-          variants={{
-            appearing: {
-              x: -250,
-              transition: {
-                ease: easeInOutExpo,
-                duration: 0.8,
-              },
-            },
-            visible: {
-              x: 0,
-              transition: {
-                ease: easeInOutExpo,
-                duration: 0.8,
-              },
-            },
-          }}
-        >
-          <Box component="nav">
-            <SwipeableDrawer
-              open={isSidebarOpen}
-              onOpen={isSidebarOpen}
-              onClose={() => setIsSidebarOpen(false)}
-              variant="persistent"
-              anchor="left"
-              sx={{
-                width: drawerWidth,
-                "& .MuiDrawer-paper": {
-                  color: theme.palette.secondary[200],
-                  backgroundColor: theme.palette.background.alt,
-                  boxSizing: "border-box",
-                  borderWidth: isNonMobile ? 0 : "2px",
-                  width: drawerWidth,
-                  overflow: "hidden",
+    <>
+      {isNonMobile ? (
+        <AnimatePresence>
+          {isSidebarOpen ? (
+            <motion.div
+              key="sidebarContent"
+              initial="appearing"
+              animate="visible"
+              exit="appearing"
+              variants={{
+                appearing: {
+                  x: -250,
+                  transition: {
+                    ease: easeInOutExpo,
+                    duration: 0.8,
+                  },
+                },
+                visible: {
+                  x: 0,
+                  transition: {
+                    ease: easeInOutExpo,
+                    duration: 0.8,
+                  },
                 },
               }}
             >
-              <Box width="100%">
-                <Box m="1.5rem 2rem 2rem 3rem">
-                  <FlexBetween color={theme.palette.secondary.main}>
-                    <Box display="flex" alignItems="center" gap="0.5rem">
-                      <Typography variant="h4" fontWeight="bold">
-                        BOARDASH
-                      </Typography>
-                    </Box>
-                    {!isNonMobile ? (
-                      <IconButton
-                        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-                      >
-                        <ChevronLeft />
-                      </IconButton>
-                    ) : null}
-                  </FlexBetween>
-                </Box>
-                <List sx={{ overflow: "hidden" }}>
-                  {navItems.map(({ text, icon }) => {
-                    if (!icon) {
-                      return (
-                        <Typography
-                          key={text}
-                          sx={{ m: "2.25rem 0 1rem 3rem" }}
-                        >
-                          {text}
-                        </Typography>
-                      );
-                    }
-                    const lcText = text.toLowerCase();
-                    return (
-                      <MotionBox key={text}>
-                        <ListItem disablePadding>
-                          <ListItemButton
-                            onClick={() => {
-                              router.push(`/${lcText}`);
-                              setActive(lcText);
-                            }}
-                            sx={{
-                              backgroundColor:
-                                active === lcText
-                                  ? theme.palette.secondary[300]
-                                  : "transparent",
-                              color:
-                                active === lcText
-                                  ? theme.palette.primary[600]
-                                  : theme.palette.secondary[100],
-                            }}
-                          >
-                            <ListItemIcon
-                              sx={{
-                                ml: "2rem",
-                                color:
-                                  active === lcText
-                                    ? theme.palette.primary[600]
-                                    : theme.palette.secondary[200],
-                              }}
-                            >
-                              {icon}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                            {active === lcText ? (
-                              <ChevronRightOutlined sx={{ ml: "auto" }} />
-                            ) : null}
-                          </ListItemButton>
-                        </ListItem>
-                      </MotionBox>
-                    );
-                  })}
-                </List>
-              </Box>
-              <Box position="absolute" bottom="2rem">
-                <Divider />
-                <FlexBetween
-                  textTransform="none"
-                  gap="1rem"
-                  m="1.5rem 2rem 0 3rem"
+              <Box component="nav">
+                <Drawer
+                  open={isSidebarOpen}
+                  onClose={() => setIsSidebarOpen(false)}
+                  variant="persistent"
+                  anchor="left"
+                  sx={{
+                    width: drawerWidth,
+                    "& .MuiDrawer-paper": {
+                      color: theme.palette.secondary[200],
+                      backgroundColor: theme.palette.background.alt,
+                      boxSizing: "border-box",
+                      borderWidth: 0,
+                      width: drawerWidth,
+                      overflow: "hidden",
+                    },
+                  }}
                 >
-                  <Box
-                    component="img"
-                    alt="profile"
-                    src={profileImage.src}
-                    height="40px"
-                    width="40px"
-                    borderRadius="50%"
-                    sx={{ objectFit: "cover" }}
-                  />
-                  <Box textAlign="left">
-                    <Typography
-                      fontWeight="bold"
-                      fontSize="0.9rem"
-                      sx={{ color: theme.palette.secondary[100] }}
-                    >
-                      {user.name}
-                    </Typography>
-                    <Typography
-                      fontSize="0.8rem"
-                      sx={{ color: theme.palette.secondary[200] }}
-                    >
-                      {user.occupation}
-                    </Typography>
+                  <Box width="100%">
+                    <Box m="1.5rem 2rem 2rem 3rem">
+                      <FlexBetween color={theme.palette.secondary.main}>
+                        <Box display="flex" alignItems="center" gap="0.5rem">
+                          <Typography variant="h4" fontWeight="bold">
+                            BOARDASH
+                          </Typography>
+                        </Box>
+                      </FlexBetween>
+                    </Box>
+                    <List sx={{ overflow: "hidden" }}>
+                      {navItems.map(({ text, icon }) => {
+                        if (!icon) {
+                          return (
+                            <Typography
+                              key={text}
+                              sx={{ m: "2.25rem 0 1rem 3rem" }}
+                            >
+                              {text}
+                            </Typography>
+                          );
+                        }
+                        const lcText = text.toLowerCase();
+                        return (
+                          <MotionBox key={text}>
+                            <ListItem disablePadding>
+                              <ListItemButton
+                                onClick={() => {
+                                  router.push(`/${lcText}`);
+                                  setActive(lcText);
+                                }}
+                                sx={{
+                                  backgroundColor:
+                                    active === lcText
+                                      ? theme.palette.secondary[300]
+                                      : "transparent",
+                                  color:
+                                    active === lcText
+                                      ? theme.palette.primary[600]
+                                      : theme.palette.secondary[100],
+                                }}
+                              >
+                                <ListItemIcon
+                                  sx={{
+                                    ml: "2rem",
+                                    color:
+                                      active === lcText
+                                        ? theme.palette.primary[600]
+                                        : theme.palette.secondary[200],
+                                  }}
+                                >
+                                  {icon}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                                {active === lcText ? (
+                                  <ChevronRightOutlined sx={{ ml: "auto" }} />
+                                ) : null}
+                              </ListItemButton>
+                            </ListItem>
+                          </MotionBox>
+                        );
+                      })}
+                    </List>
                   </Box>
-                  <MotionBox>
-                    <IconButton>
-                      <SettingsOutlined
-                        sx={{
-                          color: theme.palette.secondary[300],
-                          fontSize: "25px",
-                        }}
+                  <Box position="absolute" bottom="2rem">
+                    <Divider />
+                    <FlexBetween
+                      textTransform="none"
+                      gap="1rem"
+                      m="1.5rem 2rem 0 3rem"
+                    >
+                      <Box
+                        component="img"
+                        alt="profile"
+                        src={profileImage.src}
+                        height="40px"
+                        width="40px"
+                        borderRadius="50%"
+                        sx={{ objectFit: "cover" }}
                       />
-                    </IconButton>
-                  </MotionBox>
-                </FlexBetween>
+                      <Box textAlign="left">
+                        <Typography
+                          fontWeight="bold"
+                          fontSize="0.9rem"
+                          sx={{ color: theme.palette.secondary[100] }}
+                        >
+                          {user.name}
+                        </Typography>
+                        <Typography
+                          fontSize="0.8rem"
+                          sx={{ color: theme.palette.secondary[200] }}
+                        >
+                          {user.occupation}
+                        </Typography>
+                      </Box>
+                      <MotionBox>
+                        <IconButton>
+                          <SettingsOutlined
+                            sx={{
+                              color: theme.palette.secondary[300],
+                              fontSize: "25px",
+                            }}
+                          />
+                        </IconButton>
+                      </MotionBox>
+                    </FlexBetween>
+                  </Box>
+                </Drawer>
               </Box>
-            </SwipeableDrawer>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
+      ) : (
+        <SwipeableDrawer
+          open={isSidebarOpen}
+          onOpen={() => setIsSidebarOpen(true)}
+          onClose={() => setIsSidebarOpen(false)}
+          anchor="left"
+          swipeAreaWidth={28}
+          disableSwipeToOpen={false}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.alt,
+              boxSizing: "border-box",
+              borderWidth: "2px",
+              width: drawerWidth,
+              overflow: "hidden",
+              overflowY: "visible",
+            },
+          }}
+        >
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.secondary.main}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    BOARDASH
+                  </Typography>
+                </Box>
+                <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                  <ChevronLeft />
+                </IconButton>
+              </FlexBetween>
+            </Box>
+            <List sx={{ overflow: "hidden" }}>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLowerCase();
+                return (
+                  <MotionBox key={text}>
+                    <ListItem disablePadding>
+                      <ListItemButton
+                        onClick={() => {
+                          router.push(`/${lcText}`);
+                          setActive(lcText);
+                        }}
+                        sx={{
+                          backgroundColor:
+                            active === lcText
+                              ? theme.palette.secondary[300]
+                              : "transparent",
+                          color:
+                            active === lcText
+                              ? theme.palette.primary[600]
+                              : theme.palette.secondary[100],
+                        }}
+                      >
+                        <ListItemIcon
+                          sx={{
+                            ml: "2rem",
+                            color:
+                              active === lcText
+                                ? theme.palette.primary[600]
+                                : theme.palette.secondary[200],
+                          }}
+                        >
+                          {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                        {active === lcText ? (
+                          <ChevronRightOutlined sx={{ ml: "auto" }} />
+                        ) : null}
+                      </ListItemButton>
+                    </ListItem>
+                  </MotionBox>
+                );
+              })}
+            </List>
           </Box>
-        </motion.div>
-      ) : null}
-    </AnimatePresence>
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage.src}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <MotionBox>
+                <IconButton>
+                  <SettingsOutlined
+                    sx={{
+                      color: theme.palette.secondary[300],
+                      fontSize: "25px",
+                    }}
+                  />
+                </IconButton>
+              </MotionBox>
+            </FlexBetween>
+          </Box>
+        </SwipeableDrawer>
+      )}
+    </>
   );
 };
 
